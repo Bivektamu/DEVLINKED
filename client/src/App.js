80507b85a95371651bcc59796627e932e57cd68e
Bivek Gurung <bivek.tamu@gmail.com>
2020-01-06 14:25:51 +1100

    Boilerplate for landing and Nav component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import React, { Fragment } from 'react';
+import NavBar from './components/layout/NavBar';
+import Landing from './components/layout/Landing';
 // import logo from './logo.svg';
 import './App.css';
 
 const App = () => (
   <Fragment>
-    <h1>App</h1>
+    <NavBar />
+    <Landing />
   </Fragment>
 );
 
