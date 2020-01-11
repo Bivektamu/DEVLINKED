38e9bf3a97011d8c1ac54e544dac71e5fea99e55
Bivek Gurung <bivek.tamu@gmail.com>
2020-01-12 01:00:47 +1100

    Create profile page completed for front end as well

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 // import Default from './components/Default';
 import Dashboard from './components/dashboard/Dashboard';
+import CreateProfile from './components/profile-forms/CreateProfile';
 
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import PrivateRoute from './components/routing/PrivateRoute';
@@ -37,6 +38,11 @@ const App = () => {
           <Route exact path='/register' component={Register} />
           <Route exact path='/login' component={Login} />
           <PrivateRoute exact path='/dashboard' component={Dashboard} />
+          <PrivateRoute
+            exact
+            path='/create-profile'
+            component={CreateProfile}
+          />
         </Switch>
       </Router>
     </Provider>
